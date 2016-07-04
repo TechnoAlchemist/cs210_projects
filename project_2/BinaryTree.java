b3433de0df7b53066a002512b87d704a559dd0b7
Gabe <glafonta@gmail.com>
2016-07-03 23:04:48 -0400

    Binary Tree with methods for post, pre and inorder traversal as well as rotate left and right methods

--- project_2/BinaryTree.java
+++ project_2/BinaryTree.java
@@ -8,45 +8,63 @@ public class BinaryTree
 		 Node left; 
 		 Node right; 
 		 Node(int data, int N) { 
-			.....
+		     this.data = data;
+		     this.N = N;
 		 } 
 	 }  
 	 // return true if this symbol table is empty; 
 	 public boolean isEmpty() {
-	        .....
+	     return size() == 0;
 	    }
 	     
 	//Returns the number of key-value pairs in this symbol table.  
 	 public int size() {
-	        .....
+	     return size(rootNode);
 	    }
 	 
 	 // return number of key-value pairs in BST rooted at x
 	    public static int size(Node x) {
-	       .....
+		if (x == null) { return 0; }
+		else { return x.N; }
 	    } 
 	    
 	// In order code    
  	public void inOrder(Node root) { 
- 		......
- 		}
+	    if (root == null) { return; }
+	    inOrder(root.left);
+	    System.out.println(root.data);
+	    inOrder(root.right);
+ 	}
  	//Pre order code
  	public void preOrder(Node root){
- 		.......
+	    if (root == null) { return; }
+	    System.out.println(root.data);
+	    preOrder(root.left);
+	    preOrder(root.right);
+	    
  	}
  	//Post order code
  	public void postOrder(Node root){
- 		.......
+	    if (root == null) { return; }
+	    postOrder(root.left);
+	    postOrder(root.right);
+	    System.out.println(root.data);
  	}
  	// left-leaning link lean to the right
  	private Node rotateRight(Node h) {
-        .......
-    }
+	    Node x = h.left;
+	    h.left = x.right;
+	    x.right = h;
+	    return x;
+	}
  	
  	  // right-leaning link lean to the left
-    private Node rotateLeft(Node h) {
-       ........
-    }
+       private Node rotateLeft(Node h) {
+	   Node x = h.right;
+	   h.right = x.left;
+	   x.left = h;
+	   return x;
+       }
  	
  	public static void main(String[] args) { 
  		BinaryTree bi=new BinaryTree(); // Creating a binary tree 
